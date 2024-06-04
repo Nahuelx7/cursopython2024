88b2fe65e9b3eece09c7269f4b7f072d67f677b4
Enzo Nahuel Sanchez <nahuel.enzo.sanchez@gmail.com>
2024-06-04 20:12:17 -0300

    primer subida a git hub

---
+++ clase4_junio.py
@@ -0,0 +1,11 @@
+#podes comentar el codigo con el "#" adelante para una sola linea
+""" 
+O podes utilizar las triples comillas para comentar multinea
+como
+en
+este
+ejemplo
+"""
+
+print(1+2+3)
+print(3*3)
